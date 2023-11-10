8baa8015341cc60233c4bf6377aa8e55f98a6457
HanbiHeo <hhb0365@gmail.com>
2023-11-10 10:23:38 +0900

    first commit hanbi

---
+++ src/main/java/com/dobby/dobby/vo/BoardVO.java
@@ -0,0 +1,18 @@
+package com.dobby.dobby.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+import java.sql.Date;
+
+@Getter
+@Setter
+public class BoardVO {
+    private String major;
+    private String sub;
+    private Long boardId;
+    private String userId;
+    private String title;
+    private String content;
+    private String img;
+    private Date regDate;
+}
