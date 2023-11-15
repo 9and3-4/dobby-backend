81f6e89efc3a5d06fa22c4fea89895dafa268ce8
HyunSu1210 <hyunsu015@gmail.com>
2023-11-16 08:42:58 +0900

    이메일 인증 추가

--- src/main/java/com/dobby/dobby/controller/EmailController.java
+++ src/main/java/com/dobby/dobby/controller/EmailController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.UUID;
@@ -28,7 +29,7 @@ public class EmailController {
     private JavaMailSender mailSender;
 
     @GetMapping("/mail")
-    public ResponseEntity<Boolean> sendMail(@RequestParam String id) {
+    public ResponseEntity<String> sendMail(@RequestParam String id) {
         boolean isSent = false;
         System.out.println("인증 번호 받을 email : " + id);
         // 임의의 인증 번호 생성
@@ -39,10 +40,13 @@ public class EmailController {
         System.out.println("인증 번호 : " + tempPw);
 
         // 이메일에 들어갈 내용
-        String htmlContent = "<div style=\"text-align: center; display:flex; justify-contents:center; text-align:center;\">"
-                + "<p style=\"font-size: 16px;\">9와 3/4 승강장 이메일 인증 번호입니다.</p>"
-                + "<div style=\"font-size:20px; font-style:bold; width: 100px; height:50px; border: 1px solid #c6c6c6;\">" + tempPw + "</div>"
-                + "<p style=\"font-size: 12px;\">인증 번호를 입력해주세요.</p>"
+        String htmlContent = "<div style=\"text-align: center; display:flex; flex-direction:column; justify-content:center; text-align:center;\">"
+                + "<p style=\"font-size:30px; display: block;\">9와 3/4 승강장에 오신 것을 환영합니다.</p>"
+                + "<p></p>"
+                + "<p style=\"font-size:16px; display: block;\">인증 번호를 입력해야 알려지지 않은 통로에 입장하실 수 있습니다.</p>"
+                + "<p></p>"
+                + "<div style=\"font-size:20px; font-style:bold; width: 1000px; height:50px; border: 1px solid #c6c6c6; display: block;\">" + tempPw + "</div>"
+                + "<img src=\"https://mblogthumb-phinf.pstatic.net/MjAyMDA1MTRfODYg/MDAxNTg5NDIzNTg0MDY0.0FjhM3Q4mAmOLCrs1ohYnKSZsqbfCVQmerKFpJlppBMg.o5qWKthkRltq0v3GI4EjT_rl9nCa8ndgiojftUMJTDgg.PNG.gustn0020420/image.png?type=w800\" style=\"width: 1000px; height:400px; border: 1px solid #c6c6c6;\">"
                 + "</div>";
 
         // 임시 비밀번호를 이메일로 전송
@@ -52,7 +56,7 @@ public class EmailController {
         try {
             helper.setFrom("dobby22023@naver.com");
             helper.setTo(id);
-            helper.setSubject("Dobby 이메일 인증");
+            helper.setSubject("Hello, 9와 3/4 승강장에 오신 것을 환영합니다.");
             helper.setText(htmlContent, true);
         } catch (MessagingException e) {
             e.printStackTrace();
@@ -61,6 +65,7 @@ public class EmailController {
         isSent = true;
         System.out.println("isSent : " + isSent);
 
-    return new ResponseEntity<>(isSent,HttpStatus.OK);
+
+        return new ResponseEntity<>(tempPw,HttpStatus.OK);
     }
 }
